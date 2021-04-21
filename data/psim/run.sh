95df35ac942437d051cdedc996e7f43ca8264747
minwei.jiang <minwei.jiang@autocore.ai>
2021-04-21 11:14:25 +0800

    doc: ACFS-286: Released new version 2.0

--- data/psim/run.sh
+++ data/psim/run.sh
@@ -2,6 +2,4 @@
 
 source /AutowareArchitectureProposal/install/setup.bash
 
-bash -c "/data/psim/engage.sh" &
 
-ros2 launch autoware_launch planning_simulator.launch.xml map_path:=/data/psim vehicle_model:=lexus sensor_model:=aip_xx1
